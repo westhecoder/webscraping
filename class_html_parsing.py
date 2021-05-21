e9a1bd15054e0ee9be6504549fbec5dcaf0470d5
wesley <weslmusek@gmail.com>
2021-05-21 12:51:16 +0200

    final stage

--- class_html_parsing.py
+++ class_html_parsing.py
@@ -47,51 +47,71 @@ ITEM_HTML = '''<!DOCTYPE html>
 </html>
 '''
 
-soup = BeautifulSoup(ITEM_HTML, 'html.parser')
+class ParsedItemLocators:
+
+    '''
+    Locators for an item in html page
+    This allows us to see what our code will be looking at as well as change it
+    quickly if we notice it is now different.
+    '''
+
+    NAME_LOCATOR = 'article.product_pod h3 a'
+    LINK_LOCATOR = 'article.product_pod h3 a'
+    PRICE_LOCATOR = 'article.product_pod p.price_color'
+    RATING_LOCATOR = 'article.product_pod p.star-rating'
 
 
-def find_item_name():
-    locator = 'article.product_pod h3 a'  # CSS locator, location of what we want
-    item_link = soup.select_one(locator)
-    item_name = item_link.attrs['title']
-    print(item_name)
+
+class ParsedItem:
+
+    '''
+    a class to take an HTML page or part of it and find properties in it.
+    '''
+
+    def __init__(self, page):
+        self.soup = BeautifulSoup(page, 'html.parser')
 
 
-def find_item_link():
-    locator = 'article.product_pod h3 a'  # CSS locator, location of what we want
-    item_link = soup.select_one(locator).attrs['href']  # this is a relative link
-    print(item_link)
+    def find_item_name(self):
+        locator =   ParsedItemLocators.NAME_LOCATOR # CSS locator, location of what we want
+        item_link = self.soup.select_one(locator)
+        item_name = item_link.attrs['title']
+        return item_name
 
 
-'''
-def find_item_price():
-    # locator = 'article.product_pod p.price_color'
-    # item_link = soup.select_one(locator)
-    prod_price = soup.find('p', {'class': 'price_color'}).string
-    print(prod_price[1:])  # this extracts a strong not a number
-'''
+    def find_item_link(self):
+        locator =  ParsedItemLocators.LINK_LOCATOR# CSS locator, location of what we want
+        item_link = self.soup.select_one(locator).attrs['href']  # this is a relative link
+        return item_link
 
 
-def find_item_price():
-    locator = 'article.product_pod p.price_color'
-    item_price = soup.select_one(locator).string
-
-    pattern = '£([0-9]+\.[0-9]+)'
-    matcher = re.search(pattern, item_price)
-    print(matcher.group(0))  # £51.77
-    print(float(matcher.group(1)))  # 51.77
-
-
-def find_item_rating():
-    locator = 'article.product_pod p.star-rating'
-    star_rating_tag = soup.select_one(locator)
-    # print(star_rating_tag)
-    classes = star_rating_tag.attrs['class']
-    rating_classes = [r for r in classes if r != 'star-rating']
-    print(rating_classes[0])
+    '''
+    def find_item_price(self):
+        # locator = 'article.product_pod p.price_color'
+        # item_link = self.soup.select_one(locator)
+        prod_price = soup.find('p', {'class': 'price_color'}).string
+        return prod_price[1:]  # this extracts a strong not a number
+    '''
 
 
-find_item_name()
-find_item_link()
-find_item_price()
-find_item_rating()
+    def find_item_price(self):
+        locator = ParsedItemLocators.PRICE_LOCATOR
+        item_price = self.soup.select_one(locator).string
+
+        pattern = '£([0-9]+\.[0-9]+)'
+        matcher = re.search(pattern, item_price)
+        return matcher.group(0)  # £51.77
+        return float(matcher.group(1))  # 51.77
+
+
+    def find_item_rating(self):
+        locator = ParsedItemLocators.PRICE_LOCATOR
+        star_rating_tag = soup.select_one(locator)
+        # print(star_rating_tag)
+        classes = star_rating_tag.attrs['class']
+        rating_classes = [r for r in classes if r != 'star-rating']
+        return rating_classes[0]
+
+# ecapsulation
+item = ParsedItem(ITEM_HTML)
+print(item.find_item_link())
