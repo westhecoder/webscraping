ec4ead2c4ba9939adb31732ca1b87d2dc8d6bc02
wesley <weslmusek@gmail.com>
2021-05-26 10:05:57 +0200

    working_quote_code

--- scraping-quotes/pages/quotes_page.py
+++ scraping-quotes/pages/quotes_page.py
@@ -1,6 +1,6 @@
 from bs4 import BeautifulSoup
 
-from locators.quotes_page_locators import QuotesPageLocators
+from  locators.quotes_page_locators import QuotesPageLocators
 from parsers.quote import QuoteParser
 
 class QuotesPage:
